e3b1407e47838d09faed1a99f5b22700236dd4e3
Daniel Legan <legan.daniel@gmail.com>
2023-01-23 13:12:13 -0600

    more challenges

--- code-challenges.js
+++ code-challenges.js
@@ -55,3 +55,35 @@ function evenOrOdd(number) {
         return 'Odd'
     }
 }
+
+/* Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times. */
+
+/* 
+"I"     -> "IIIIII"
+"Hello" -> "HelloHelloHelloHelloHello"
+*/
+
+function repeatStr(n, s) {
+    return s.repeat(n);
+}
+
+/* 
+What if we need the length of the words separated by a space to be added at the end of that same word and have it returned as an array?
+
+Example(Input --> Output)
+
+"apple ban" --> ["apple 5", "ban 3"]
+"you will win" -->["you 3", "will 4", "win 3"]
+Your task is to write a function that takes a String and returns an Array/List with the length of each word added to each element.
+
+Note: String will have at least one element; words will always be separated by a space.
+*/
+
+function addLength(str) {
+    let arr = [];
+    let words = str.split(' ');
+    for (let word of words) {
+        arr.push(word + ' ' + word.length);
+    }
+    return arr;
+}
