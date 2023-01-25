fe2d1f47242ae6f67da7116c71451230839f3504
Daniel Legan <legan.daniel@gmail.com>
2023-01-25 14:39:43 -0600

    more challenges

--- code-challenges.js
+++ code-challenges.js
@@ -116,6 +116,10 @@ function isSquare(n) {
     return Number.isInteger(Math.sqrt(n));
 }
 
+var isSquare = function (n) {
+    return Math.sqrt(n) % 1 === 0 ? true : false;
+}
+
 /*
 Given a set of numbers, return the additive inverse of each.
 Each positive becomes negatives, and the negatives become positives.
@@ -145,6 +149,15 @@ var number = function (busStops) {
         people += stop[0] - stop[1];
     }
     return people;
+}
+
+var number = function (busStops) {
+    let result = 0;
+    for (let i = 0; i < busStops.length; i++) {
+        result += busStops[i][0]
+        result -= busStops[i][1]
+    }
+    return result;
 }
 
 /*
@@ -234,3 +247,28 @@ var twoSum = function (nums, target) {
         }
     }
 };
+
+/* 
+Trolls are attacking your comment section!
+
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+*/
+function disemvowel(str) {
+    let result = '';
+    const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+    for (let i = 0; i < str.length; i++) {
+        if (!vowels.includes(str[i])) {
+            result += str[i]
+        }
+    }
+    return result;
+}
+
+// from someone else
+function disemvowel(str) {
+    return str.replace(/[aeiou]/gi, '');
+}
