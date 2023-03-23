b92355b47502e28836ac6b5b5cdef17d410ab87b
Daniel Legan <legan.daniel@gmail.com>
2023-03-23 16:47:46 -0400

    challenge done

--- code-challenges.js
+++ code-challenges.js
@@ -373,3 +373,32 @@ function solution(str) {
 
     return result;
 }
+
+/*
+Given an array of integers, find the one that appears an odd number of times.
+
+There will always be only one integer that appears an odd number of times.
+
+Examples
+[7] should return 7, because it occurs 1 time (which is odd).
+[0] should return 0, because it occurs 1 time (which is odd).
+[1,1,2] should return 2, because it occurs 1 time (which is odd).
+[0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+[1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+*/
+
+function findOdd(arr) {
+    let counts = {};
+    for (let i = 0; i < arr.length; i++) {
+        if (counts[arr[i]]) {
+            counts[arr[i]]++;
+        } else {
+            counts[arr[i]] = 1;
+        }
+    }
+    for (let key in counts) {
+        if (counts[key] % 2 !== 0) {
+            return parseInt(key);
+        }
+    }
+}
