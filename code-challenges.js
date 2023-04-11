acd87b3ccb26f247e95b1d5c105356f67dd9b4c7
Daniel Legan <legan.daniel@gmail.com>
2023-04-11 17:15:25 -0400

    challenge done

--- code-challenges.js
+++ code-challenges.js
@@ -402,3 +402,36 @@ function findOdd(arr) {
         }
     }
 }
+
+/*
+Count the number of Duplicates
+Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+Example
+"abcde" -> 0 # no characters repeats more than once
+"aabbcde" -> 2 # 'a' and 'b'
+"aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+"indivisibility" -> 1 # 'i' occurs six times
+"Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+"aA11" -> 2 # 'a' and '1'
+"ABBA" -> 2 # 'A' and 'B' each occur twice
+*/
+
+function duplicateCount(str) {
+    // Convert the string to lowercase and split it into an array of characters
+    const chars = str.toLowerCase().split('');
+    // Count the frequency of each character using an object
+    const frequency = {};
+    for (let i = 0; i < chars.length; i++) {
+        const char = chars[i];
+        frequency[char] = (frequency[char] || 0) + 1;
+    }
+    // Count the number of characters that occur more than once
+    let count = 0;
+    for (const char in frequency) {
+        if (frequency[char] > 1) {
+            count++;
+        }
+    }
+    return count;
+}
