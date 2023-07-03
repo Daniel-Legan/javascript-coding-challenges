5376f100da05bc430628a9ce6af2f513943ab2a1
Daniel Legan <legan.daniel@gmail.com>
2023-07-03 17:18:36 -0400

    code challenge done

--- code-challenges.js
+++ code-challenges.js
@@ -449,3 +449,28 @@ function reverseSeq(n) {
     }
     return result;
 }
+
+/*
+Write a function named sumDigits which takes a number as input and returns the sum of the absolute value of each of the number's decimal digits.
+
+For example: (Input --> Output)
+
+10 --> 1
+99 --> 18
+-32 --> 5
+
+Let's assume that all numbers in the input will be integer values.
+*/
+
+function sumDigits(num) {
+    const absoluteNum = Math.abs(num);
+
+    if (absoluteNum < 10) {
+        return absoluteNum;
+    }
+
+    const lastDigit = absoluteNum % 10;
+    const remainingNum = Math.floor(absoluteNum / 10);
+
+    return lastDigit + sumDigits(remainingNum);
+}
