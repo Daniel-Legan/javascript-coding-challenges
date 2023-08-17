bda967d25dc5a22054911af22a22b45e787b4209
Daniel Legan <legan.daniel@gmail.com>
2023-08-16 23:56:03 -0400

    challenge completed

--- code-challenges.js
+++ code-challenges.js
@@ -556,3 +556,26 @@ function addedChar(str1, str2) {
 
     return sorted2[sorted2.length - 1];
 }
+
+/*
+Define a function that removes duplicates from an array of non negative numbers and returns it as a result.
+
+The order of the sequence has to stay the same.
+
+Examples:
+
+Input -> Output
+[1, 1, 2] -> [1, 2]
+[1, 2, 1, 1, 3, 2] -> [1, 2, 3]
+*/
+
+function distinct(array) {
+    const resultArray = [];
+
+    for (let element of array) {
+        if (resultArray.indexOf(element) === -1) {
+            resultArray.push(element);
+        }
+    }
+    return resultArray;
+}
