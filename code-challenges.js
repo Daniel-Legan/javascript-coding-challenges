98f00ea830ac7e6159fd06e6be515f6ce6d563b0
Daniel Legan <legan.daniel@gmail.com>
2023-10-19 19:25:20 -0400

    challenge done

--- code-challenges.js
+++ code-challenges.js
@@ -904,3 +904,43 @@ function stringBreakers(n, string) {
 }
 
 // console.log(stringBreakers(3, "This is a test string"));
+
+/*
+Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+It should remove all values from list a, which are present in list b keeping their order.
+
+arrayDiff([1,2],[1]) == [2]
+If a value is present in b, all of its occurrences must be removed from the other:
+
+arrayDiff([1,2,2,2,3],[2]) == [1,3]
+*/
+
+function arrayDiff(a, b) {
+    const result = [];
+
+    for (let i = 0; i < a.length; i++) {
+        let inB = false;
+
+        for (let j = 0; j < b.length; j++) {
+            if (a[i] === b[j]) {
+                inB = true;
+            }
+        }
+        if (!inB) {
+            result.push(a[i]);
+        }
+    }
+
+    return result;
+}
+
+/*
+A better way from Codewars submissions:
+
+function array_diff(a, b) {
+  return a.filter(e => !b.includes(e));
+}
+*/
+
+// console.log(arrayDiff([1, 1, 2, 3, 1], [1, 2]));
