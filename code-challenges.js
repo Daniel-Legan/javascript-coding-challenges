7fdaec2e8690ea92bed5a23fadd5dfe34712166b
Daniel Legan <legan.daniel@gmail.com>
2023-10-20 12:21:58 -0400

    challenge done

--- code-challenges.js
+++ code-challenges.js
@@ -944,3 +944,35 @@ function array_diff(a, b) {
 */
 
 // console.log(arrayDiff([1, 1, 2, 3, 1], [1, 2]));
+
+/*
+The argument passed into the function is an array with the format [D, M, YYYY], e.g. [1, 2, 2000] for February 1st, 2000 or [22, 12, 1999] for December 22nd, 1999.
+
+Don't forget to check for whether it's a leap year! Three criteria must be taken into account to identify leap years:
+
+The year can be evenly divided by 4;
+If the year can be evenly divided by 100, it is NOT a leap year, unless;
+The year is also evenly divisible by 400. Then it is a leap year.
+*/
+
+function toDayOfYear(arr) {
+    const day = arr[0];
+    const month = arr[1];
+    const year = arr[2];
+
+    const daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    let numberOfDays = day;
+    
+    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+        daysInMonth[1] = 29;
+    }
+
+    for (let i = 0; i < month - 1; i++) {
+        numberOfDays += daysInMonth[i];
+    }
+
+    return numberOfDays;
+}
+
+// console.log(toDayOfYear([1, 2, 2000]));
