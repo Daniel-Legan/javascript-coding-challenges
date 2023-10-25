e088463b316c2494440bd4bcb7acba70c64e4cd4
Daniel Legan <legan.daniel@gmail.com>
2023-10-25 15:33:31 -0400

    challenge done

--- code-challenges.js
+++ code-challenges.js
@@ -1015,3 +1015,70 @@ function solve(arr) {
 }
 
 // console.log(solve([3, 4, 4, 3, 6, 3]));
+
+/*
+If you're not familiar with the fantastic culinary delights of the British Isles you may not know about the bread sandwich.
+
+The idea is very simple - if you have a slice of bread between two other slices of bread, then it's a bread sandwich. Additionally, if you have a bread sandwich between two other slices of bread, you get a bread sandwich sandwich, and so on.
+
+In this kata, we will define the following terms like so:
+
+Sandwich - Two slices of bread which may or may not have a filling
+Bread Sandwich - Two slices of bread which contains one slice of bread in the middle as a filling
+You will need to build two functions:
+
+Given the number of slices of bread, return the phrase used to refer to the sandwich
+ 2 - 'sandwich'
+ 5 - 'bread sandwich sandwich'
+The reverse function - given the name of the sandwich, return how many slices of bread there are in the sandwich
+'bread sandwich' - 3
+'sandwich sandwich' - 4
+You should return None/null if there is no input / the input is invalid / there is no sandwich
+Maximum 100 slices of bread
+*/
+
+function slicesToName(n) {
+    let breadToAdd = n % 2 === 0 ? false : true;
+
+    if (n > 1 && breadToAdd === false) {
+        let count = n;
+        let phrase = [];
+        for (let i = 0; i < count / 2; i++) {
+            phrase.push('sandwich');
+        }
+        return phrase.join(' ').trim();
+    } else if (n > 1 && breadToAdd === true) {
+        let count = n - 1;
+        let phrase = ['bread'];
+        for (let i = 0; i < count / 2; i++) {
+            phrase.push('sandwich');
+        }
+        return phrase.join(' ').trim();
+    } else {
+        return null;
+    }
+}
+
+function nameToSlices(name) {
+    if (!name || typeof name !== 'string') {
+        return null;
+    }
+
+    if (name === "bread") {
+        return null;
+    }
+
+    const sandwichNames = name.split(' ');
+
+    let slices = 1;
+    for (i = 1; i < sandwichNames.length; i++) {
+        if (sandwichNames[i] === 'bread') {
+            return null
+        }
+        slices += 2;
+    }
+    return slices;
+}
+
+// console.log(slicesToName(3));
+// console.log(nameToSlices('bread sandwich'));
